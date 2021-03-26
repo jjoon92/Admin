7e135e3dd399b53f614b987cc2a2259980590d13
jjoon92 <jjoon92@naver.com>
2021-03-26 17:38:53 +0900

    JPA를 통한 CRUD 배우기-2

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -1,17 +1,20 @@
 package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
-
 import com.example.study.model.entity.User;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public class UserRepositoryTest extends StudyApplicationTests
-{
+
+
+
+public class UserRepositoryTest extends StudyApplicationTests {
 
     //Dependency Injection(DI)
 
@@ -19,39 +22,40 @@ public class UserRepositoryTest extends StudyApplicationTests
     private UserRepository userRepository;
 
     @Test
-    public void create(){
-    //String sql = insert into user(%s, %s, %d) value(account, email,age);
+    public void create() {
+        //String sql = insert into user(%s, %s, %d) value(account, email,age);
         User user = new User();
 
-        user.setAccount("TestUser03");
-        user.setEmail("TestUser03@gmail.com");
+        user.setAccount("TestUser04");
+        user.setEmail("TestUser04@gmail.com");
         user.setPhoneNumber("010-1111-3333");
         user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("TestUser3");
+        user.setCreatedBy("TestUser4");
 
         User newUser = userRepository.save(user);
-        System.out.println("newUser : "+newUser);
+        System.out.println("newUser : " + newUser);
     }
 
     @Test
-    public void read(){
+    public void read() {
         Optional<User> user = userRepository.findById(2L);
 
-        user.ifPresent(selectUser->{
+        user.ifPresent(selectUser -> {
             System.out.println("user : " + selectUser);
-            System.out.println("email : "+selectUser.getEmail());
-                });
+            System.out.println("email : " + selectUser.getEmail());
+        });
 
 
     }
 
     @Test
-    public void update(){
-        
-        Optional<User> user = userRepository.findById(2L);
+    @Transactional
+    public void update() {
 
-        user.ifPresent(selectUser->{
-            selectUser.setAccount("PPPP");
+        Optional<User> user = userRepository.findById(1L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("PPPt");
             selectUser.setUpdatedAt(LocalDateTime.now());
             selectUser.setUpdatedBy("update method()");
 
@@ -61,8 +65,22 @@ public class UserRepositoryTest extends StudyApplicationTests
 
     }
 
-    public void delete(){
+    @Test
+    @Transactional
+    public void delete() {
+        Optional<User> user = userRepository.findById(1L);
+
+        Assert.assertTrue(user.isPresent()); //true
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+
+
+        });
+
+        Optional<User> deleteUser = userRepository.findById(1L);
+
+        Assert.assertFalse(deleteUser.isPresent()); //false
 
     }
-
 }
