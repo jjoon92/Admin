7080e63dd3b2941245a2c54437098dcf23c6eef4
jjoon92 <jjoon92@naver.com>
2021-03-29 16:06:17 +0900

    category user 테이블 테스트

---
+++ src/test/java/com/example/study/repository/CategoryRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.Category;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class CategoryRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void create(){
+        String type="COMPUTER";
+        String title="컴퓨터";
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
+
+        Category category = new Category();
+        category.setType(type);
+        category.setTitle(title);
+        category.setCreatedAt(createdAt);
+        category.setCreatedBy(createdBy);
+
+        Category newCategory = categoryRepository.save(category);
+        Assert.assertNotNull(newCategory);
+        Assert.assertEquals(newCategory.getType(),type);
+        Assert.assertEquals(newCategory.getTitle(),title);
+
+
+
+
+    }
+
+    public void read(){
+
+    }
+}
--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -26,33 +26,14 @@ public class UserRepositoryTest extends StudyApplicationTests {
 
     @org.junit.jupiter.api.Test
     public void create() {
-        //String sql = insert into user(%s, %s, %d) value(account, email,age);
-        User user = new User();
 
-        user.setAccount("TestUser03");
-        user.setEmail("TestUser03@gmail.com");
-        user.setPhoneNumber("010-1111-3333");
-        user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("TestUser4");
-
-        User newUser = userRepository.save(user);
-        System.out.println("newUser : " + newUser);
     }
 
     @org.junit.jupiter.api.Test
     @Transactional
     public void read() {
 
-        //select * from user where id = ?
-        Optional<User> user = userRepository.findByAccount("TestUser03");
 
-        user.ifPresent(selectUser -> {
-
-            selectUser.getOrderDetailList().stream().forEach(detail->{
-                Item item = detail.getItem();
-                System.out.println(item);
-            });
-        });
 
 
     }
