e8a982c9a104c1b324dde38db89e6b5c598338e1
jjoon92 <jjoon92@naver.com>
2021-04-11 19:12:57 +0900

    샘플 데이터 생성하기

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.Item;
 import com.example.study.model.entity.User;
 
+import com.example.study.model.enumclass.UserStatus;
 import org.junit.Assert;
 
 import org.junit.Test;
@@ -29,7 +30,7 @@ public class UserRepositoryTest extends StudyApplicationTests {
     public void create() {
         String account = "Test03";
         String password="Test03";
-        String status="REGISTERED";
+        UserStatus status=UserStatus.REGISTERED;
         String email="Test01@gmail.com";
         String phoneNumber="010-1111-3333";
         LocalDateTime registeredAt=LocalDateTime.now();
